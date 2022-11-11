ac8268ee1c462316053a4563b0194847d5615a6c
2021ht66563 <2021ht66563@wilp.bits-pilani.ac.in>
2022-11-10 19:52:04 -0500

    FileAdded

--- Base64Decoder.py
+++ Base64Decoder.py
@@ -1,4 +1,24 @@
 #
 print("bits");
 
-print("buts WILP");
+print("bu\its WILP");
+
+from base64 import *
+
+flagFile = open('encodedflag.txt', 'r').read()
+
+decodedList = [flagFile]
+
+for i in range(5):
+    decoded = b16decode(decodedList[i])
+    decodedList.append(decoded)
+
+for x in range(5,10):
+    decoded = b32decode(decodedList[x])
+    decodedList.append(decoded)
+
+for y in range(10,15):
+    decoded = b64decode(decodedList[y])
+    decodedList.append(decoded)
+
+print(decodedList[15])
